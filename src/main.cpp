10e8fd7e5a40c8bb60aa58db951ffa549a02714c
Dasioreq <adammarzec112@gmail.com>
2025-03-31 16:25:44 +0200

    Cleaned the directories up a bit and moved main.cpp to src

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include "./src/convert.hpp"
+#include <string>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "./lib/stb_image.h"
+
+int main()
+{
+    int height, width, channels;
+
+    std::string imgPath;
+    std::cout << "Input path to image (Accepted formats: .jpeg, .png, .bmp, .hdr, .psd, .tga, .gif, .pic, .pgm, .ppm): ";
+    std::cin >> imgPath;
+    std::cin.clear();
+
+    byte* buffer2 = stbi_load(imgPath.c_str(), &width, &height, &channels, 0);
+    while(buffer2 == NULL)
+    {
+        std::cout << "Failure reason: " << stbi_failure_reason() << '\n';
+        std::cout << "Invalid path, unsupported image format (.jpeg, .png, .bmp, .hdr, .psd, .tga, .gif, .pic, .pgm, .ppm) or corrupted file. Input correct path: ";
+        std::cin >> imgPath;
+        std::cin.clear();
+    
+        buffer2 = stbi_load(imgPath.c_str(), &width, &height, &channels, 0);
+    }
+
+    Image* img = new Image(buffer2, width, height, channels);
+    
+    uint16 resultChartWidth;
+    std::string validationBuffer;
+    std::cout << "Input the result image width in characters: ";
+    std::cin >> validationBuffer;
+    std::cin.clear();
+
+    resultChartWidth = std::stoi(validationBuffer);
+
+    std::cout << convert(*img, resultChartWidth);
+
+    delete[] buffer2;
+    delete img;
+}   
